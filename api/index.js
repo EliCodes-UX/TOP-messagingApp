adf2947c10435a91d72b2615423877010f56757d
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-02-26 12:41:33 -0700

    added function to register

--- api/index.js
+++ api/index.js
@@ -2,15 +2,42 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const PORT = process.env.PORT || 9040;
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const User = require('./models/User');
 
 mongoose.connect(process.env.MONGO_URL);
+const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
+app.use(express.json());
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
 
 app.get('/test', (req, res) => {
   res.json('test ok');
 });
-app.post('/register', (req, res) => {});
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const createdUser = await User.create({ username, password });
+    jwt.sign({ userId: createdUser._id }, jwtSecret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie('token', token).status(201).json({
+        _id: createdUser._id,
+      });
+    });
+  } catch (err) {
+    if (err) throw err;
+    res.status(500).json('error');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Backend server is running on http://localhost:${PORT}`);
