adc95a2d4eafa02009afbca667f60bb55b39ef8d
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-02-26 16:37:54 -0700

    need to fix in backend

---
+++ client/src/Routes.jsx
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import Register from './Register';
+import { UserContext } from './UserContext.jsx';
+
+export default function Routes() {
+  const { username, id } = useContext(UserContext);
+
+  if (username) {
+    return 'logged in';
+  }
+  return <Register />;
+}
